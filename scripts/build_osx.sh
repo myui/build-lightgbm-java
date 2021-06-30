7a940aa17c9879df3c89059b6ffaa107ce7f3a8e
Makoto Yui <myui@apache.org>
2021-06-30 16:25:48 +0900

    OSX build support

--- scripts/build_osx.sh
+++ scripts/build_osx.sh
@@ -1,8 +1,18 @@
+#!/usr/bin/env bash
+
+# Exit script if you try to use an uninitialized variable.
+set -o nounset
+# Exit script if a statement returns a non-true return value.
+set -o errexit
+
 # if [ -z "${JAVA_HOME+UNDEF}" ];then
 #   export JAVA_HOME=`/usr/libexec/java_home -v 1.7`
 # fi
 
-# export LIGHTGBM_VERSION=3.2.1
+if [ "$LIGHTGBM_VERSION" = "" ]; then
+  SCRIPT_DIR=`dirname $(realpath "$0")`
+  LIGHTGBM_VERSION=`cat $SCRIPT_DIR/../LIGHTGBM_VERSION`
+fi
 
 git clone --branch v${LIGHTGBM_VERSION} --depth 1 --recursive https://github.com/microsoft/LightGBM
 cd LightGBM
@@ -16,4 +26,5 @@ make -j4
 otool -L ../lib_lightgbm.dylib 
 otool -L ../lib_lightgbm_swig.jnilib 
 
-mv lightgbmlib.jar $TRAVIS_BUILD_DIR/lightgbm-$LIGHTGBM_VERSION-$TRAVIS_OS_NAME.jar
+mkdir -p assets
+mv lightgbmlib.jar assets/lightgbm-$LIGHTGBM_VERSION-osx.jar
